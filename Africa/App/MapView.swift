c7df1350d11fe746b911e4c30c0a2ed1f68032aa
KhuePM <minhkhuepham99@gmail.com>
2022-09-02 08:57:42 +0700

    Setup Project

---
+++ Africa/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Phạm Minh Khuê on 01/09/2022.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("MapView")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
---
+++ Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Phạm Minh Khuê on 01/09/2022.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("VideoListView")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
