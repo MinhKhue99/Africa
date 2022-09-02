99b611cc1dce75a8a9e771688e41c3542e245f01
KhuePM <minhkhuepham99@gmail.com>
2022-09-02 14:18:40 +0700

    Feature: Decode JSON file and fetch data

---
+++ Africa/Extension/CodableBundleExtension.swift
@@ -0,0 +1,28 @@
+//
+//  CodableBundleExtension.swift
+//  Africa
+//
+//  Created by Phạm Minh Khuê on 02/09/2022.
+//
+
+import Foundation
+extension Bundle {
+    func decode(_ file: String) -> [CoverImage] {
+        // 1. Locate the JSON file
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in the bundle")
+        }
+        // 2. Create the property for the data
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) in the bundle")
+        }
+        // 3. Create decoder
+        let decoder = JSONDecoder()
+        // 4. Create property for the decoded data
+        guard let loaded = try? decoder.decode([CoverImage].self, from: data) else {
+            fatalError("Failed to decode \(file) in the bundle")
+        }
+        // 5. Return the ready-to-use data
+        return loaded
+    }
+}
---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Phạm Minh Khuê on 02/09/2022.
+//
+
+import Foundation
+
+struct CoverImage: Identifiable, Codable {
+    let id: Int
+    let name: String
+}
