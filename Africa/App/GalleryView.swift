14dcf684312bbbb198ac7e96c48d46c3d5bccc95
KhuePM <minhkhuepham99@gmail.com>
2022-09-12 23:09:15 +0700

    Feature: Create Motion Animation

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct GalleryView: View {
     var body: some View {
-        Text("GalleryView")
+        ScrollView(.vertical, showsIndicators: false) {
+            Text("GalleryView")
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(MotionAnimationView())
     }
 }
 
